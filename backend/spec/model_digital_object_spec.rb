8c86fc1bc95bf3d040e1ce5059b79c0688678df4
Brian Zelip <bzelip@protonmail.com>
2022-03-03 08:02:31 -0500

    Add representative_file_version computed property to digital object model,
    Clean up digital object spec and add comments for discussion

--- backend/spec/model_digital_object_spec.rb
+++ backend/spec/model_digital_object_spec.rb
@@ -96,6 +96,7 @@ describe 'Digital object model' do
   end
 
   it "doesn't allow an unpublished file_version to be representative" do
+    # DISCUSS: if :publish is not set on each fv, it has a default value of true; if :is_representative is not set on each fv, it has a default value of false
     json = build(:json_digital_object, {
                    :publish => true,
                    :file_versions => [build(:json_file_version, {
@@ -135,17 +136,17 @@ describe 'Digital object model' do
     }.not_to raise_error
   end
 
-  it "has a representative_file_version read-only value of the file_version marked 'is_representative' if there is a file_version marked 'is_representative'" do
+  it "has a representative_file_version read-only value of the published file_version marked 'is_representative' if there is a file_version marked 'is_representative'" do
     json = build(:json_digital_object, {
                    :publish => true,
                    :file_versions => [build(:json_file_version, {
                                               :publish => true,
-                                              :is_representative => true,
                                               :file_uri => 'http://foo.com/bar1',
                                               :use_statement => 'image-service'
                                             }),
                                       build(:json_file_version, {
                                               :publish => true,
+                                              :is_representative => true,
                                               :file_uri => 'http://foo.com/bar2',
                                               :use_statement => 'image-service'
                                             })
@@ -154,11 +155,11 @@ describe 'Digital object model' do
     do1 = DigitalObject.create_from_json(json)
     do1_json = DigitalObject.to_jsonmodel(do1.id)
 
-    expect(do1_json.representative_file_version).to eq(do1_json.file_versions[0])
+    expect(do1_json.representative_file_version).to eq(do1_json.file_versions[1])
     # TODO: write representative_file_version logic
   end
 
-  it "has a representative_file_version read-only value of the first file_version with a use-statement marked 'image-thumbnail' if there is no file_version marked 'is_representative'" do
+  it "has a representative_file_version read-only value of the first published file_version with a use-statement marked 'image-thumbnail' if there is no file_version marked 'is_representative'" do
     json = build(:json_digital_object, {
                    :publish => true,
                    :file_versions => [build(:json_file_version, {
@@ -189,6 +190,7 @@ describe 'Digital object model' do
     json = build(:json_digital_object, {
                    :publish => true,
                    :file_versions => [build(:json_file_version, {
+                                              :publish => false,
                                               :file_uri => 'http://foo.com/bar1',
                                               :use_statement => 'image-service'
                                             }),
@@ -198,6 +200,7 @@ describe 'Digital object model' do
                                               :use_statement => 'image-service'
                                             }),
                                       build(:json_file_version, {
+                                              :publish => false,
                                               :file_uri => 'http://foo.com/bar3',
                                               :use_statement => 'image-thumbnail'
                                             })
@@ -210,7 +213,7 @@ describe 'Digital object model' do
     # TODO: write representative_file_version logic
   end
 
-  # TODO: Add test for the transitive requirement 4, or should this test go into PUI spec?
+  # # TODO: Add test for the transitive requirement 4, or should this test go into PUI spec?
 
   it "supports optional captions for file versions" do
     obj = create(:json_digital_object, {
