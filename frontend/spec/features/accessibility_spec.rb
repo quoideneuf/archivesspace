cc54b00f0fe929d39fce47424d2a2c86cd9615f1
Brian Hoffman <brian.hoffman@lyrasis.org>
2023-03-23 11:10:19 -0400

    Cleanup bugs preventing frontend feature tests from passing with a Chrome driver.
    Switch to Puma webserver for frontend feature tests for now. (Not totally sure
    it matters, but at least we stop seeing huge stacktraces when the browser
    hangs up on the Jetty 9 webserver, and in any case Capybara seems to prefer
    Puma as it is the default server.
    
    Add some tuning to Capybara helpers to avoid errors arising from
    slow JQuery event handlers not being attached in time for clicks
    
    add a tiny bit of proper namespacing to ead importer
    
    reconcile upgrades and master after rebase; upgrade Gemfiles
    
    remove RealtimeIndexer from feature tests
    
    upgrade puma and use single thread
    
    debug jobs_spec
    
    capture db and backend log when test fails on github
    
    direct backend log to STDOUT rather than STDERR
    
    capture backend test log without using ant record directive
    
    revert bad change to job schema

--- frontend/spec/features/accessibility_spec.rb
+++ frontend/spec/features/accessibility_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 describe 'Accessibility', js: true, db: 'accessibility' do
 
   before(:all) do
-    PeriodicIndexer.new.run_index_round
+    run_indexer
   end
 
   before(:each) do
@@ -152,13 +152,13 @@ describe 'Accessibility', js: true, db: 'accessibility' do
       page.has_css? "div.repository-header"
 
       within "div.repository-header" do
-        expect(page).not_to have_xpath("*//div[starts-with(@id,'popover')]")
+        expect(page).not_to have_xpath("*//span[starts-with(@aria-describedby,'popover')]")
         repo = find "span.repository-label"
         repo.send_keys ''
-        expect(page).to have_xpath("*//div[starts-with(@id,'popover')]")
+        expect(page).to have_xpath("*//span[starts-with(@aria-describedby,'popover')]")
 
         repo.send_keys :escape
-        expect(page).not_to have_xpath("*//div[starts-with(@id,'popover')]")
+        expect(page).not_to have_xpath("*//span[starts-with(@aria-describedby,'popover')]")
       end
     end
   end
